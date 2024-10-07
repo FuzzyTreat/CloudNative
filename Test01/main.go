314f8869aa20a3205999cd33b20a16261733ea9b
PerThoyra <per.thoyra@outlook.com>
2024-10-07 15:26:40 +0200

    New branch, Output examples

--- Test01/main.go
+++ Test01/main.go
@@ -24,7 +24,48 @@ func main() {
 	but on multiple rows */
 
 	// Variables()
-	fmt.Printf("Main Local Const value: %f\n", LOCAL_PI)
+	// fmt.Printf("Main Local Const value: %f\n", LOCAL_PI)
+
+	Output()
+}
+
+func Output() {
+	var hello = "Hello"
+	var world string = "world!"
+	var x, y = 10, 20
+	var hexValue = 65056
+
+	fmt.Println("Print with no formatting")
+	fmt.Print(hello)
+	fmt.Print(world)
+	fmt.Println("")
+
+	fmt.Print("Print with new line char\n")
+	fmt.Print(hello, "\n")
+	fmt.Print(world, "\n")
+
+	fmt.Println("Output two values in one statement")
+	fmt.Print(hello, "\n", world)
+	fmt.Println("")
+
+	fmt.Println("Output two int values in one statement, no formatting between them")
+	fmt.Print(x, y)
+	fmt.Println("")
+
+	fmt.Println("Output formatted text")
+	fmt.Printf("This text has beeen formatted with %s and %s\n", hello, world)
+
+	fmt.Printf("Const PI default format: %v\n", PI)
+	fmt.Printf("Const PI Go syntax format: %#v\n", PI)
+	fmt.Printf("Const PI type format: %T\n", PI)
+	fmt.Printf("Print the '%%', uses double %% as formatter\n")
+
+	fmt.Printf("Double quted string, no extra quotes needed: %q\n", hello)
+	fmt.Printf("Integer as a Hex dump byte values: %x\n", hexValue)
+
+	fmt.Printf("Output float value: %f\n", PI)
+	fmt.Printf("Output float value, 2 decimals: %.2f\n", PI)
+	fmt.Printf("Output float exponent as needed: %g\n", PI)
 }
 
 func Variables() {
