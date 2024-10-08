e2f36c8c29dc4fb11cea293f10cc58c7e0d39bd1
PerThoyra <per.thoyra@outlook.com>
2024-10-08 15:08:33 +0200

    Added basic index / about GET

--- goweb01/main.go
+++ goweb01/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	router := gin.Default()
+
+	router.GET("/", start)
+	router.GET("/about", about)
+	router.Run(":45000")
+}
+
+func start(c *gin.Context) {
+	c.String(200, "<h1>Hello world!</h1>")
+}
+
+func about(c *gin.Context) {
+	c.String(200, "<b>About this!</b>")
 }
