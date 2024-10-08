959edbb3fb21f062c1e43d362f0a53fa2e7327fb
PerThoyra <per.thoyra@outlook.com>
2024-10-08 15:31:38 +0200

    Added HTML example

--- goweb01/main.go
+++ goweb01/main.go
@@ -2,9 +2,17 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type PageView struct {
+	CurrentUser string
+	PageTitle   string
+	Title       string
+	Text        string
+}
+
 func main() {
 	router := gin.Default()
 
+	// router.LoadHTMLGlob("templates/**") // where are the html templates located
 	router.GET("/", start)
 	router.GET("/about", about)
 	router.Run(":45000")
@@ -12,6 +20,7 @@ func main() {
 
 func start(c *gin.Context) {
 	c.String(200, "<h1>Hello world!</h1>")
+	//c.HTML(200, "home.html", &PageView{PageTitle: "Some page", Title: "Välkommen!", Text: "Lite text på sidan"})
 }
 
 func about(c *gin.Context) {
